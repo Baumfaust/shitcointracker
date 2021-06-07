25a27f8d8f0ce5a5f00e941cf4b2401d73ebfac7
pardofelis <59371182+pardofelis@users.noreply.github.com>
2021-06-07 10:18:12 +0200

    Update to new version of main

--- main.py
+++ main.py
@@ -2,70 +2,74 @@ from collections import defaultdict
 from bscscan import BscScan
 from datetime import datetime
 from pythonpancakes import PancakeSwapAPI
-import time
+import configparser
+
+cfg = configparser.ConfigParser()
+cfg.read('config.cfg')
+
+API_KEY = cfg.get('CONFIG', 'api_key', raw='')
+ADR = cfg.get('CONFIG', 'adr', raw='')
 
 ps = PancakeSwapAPI()
-
-API_KEY = "YOUR_KEY"
-ADR = "YOUR_ADDRESS"
-
 mydict = {k: str(v).encode("utf-8") for k, v in ps.pairs().items()}
 
 all_tokens = {}
 for k, v in ps.tokens()['data'].items():
     all_tokens[k.lower()] = v
 
-with BscScan(API_KEY, asynchronous=False) as bsc: # this fixes the problem I had with "'AsyncClient' object has no attribute 'get_bnb_balance'"
+with BscScan(API_KEY, asynchronous=False) as bsc:
     balance = bsc.get_bnb_balance(address=ADR)
 
-    transfers = bsc.get_bep20_token_transfer_events_by_address(address=ADR, startblock=0, endblock=9999999999999, sort="asc")
-    normals = bsc.get_normal_txs_by_address(address=ADR, startblock=0, endblock=9999999999999, sort="asc")
+transfers = bsc.get_bep20_token_transfer_events_by_address(address=ADR, startblock=0, endblock=9999999999999,
+                                                           sort="asc")
+normals = bsc.get_normal_txs_by_address(address=ADR, startblock=0, endblock=9999999999999, sort="asc")
 
-    bnb_values = dict()
-    for n in normals:
-        bnb_values[n['hash']] = n['value']
+bnb_values = dict()
+for n in normals:
+    bnb_values[n['hash']] = n['value']
 
-    tokens = defaultdict(list)
-    for t in transfers:
-        tokenName = t['tokenName']
-        tokens[tokenName].append(t)
+tokens = defaultdict(list)
+for t in transfers:
+    tokenName = t['tokenName']
+    tokens[tokenName].append(t)
 
-    bnb_total_spend = 0
-    bnb_total_current = 0
-    print(f"{'Token':50} {'Date':30} {'Count':20} {'BNB buyin':20} {'BNB current':20} {'ROI':20}")
-    print("")
+bnb_total_spend = 0
+bnb_total_current = 0
+print(f"{'Token':50} {'Date':30} {'Count':20} {'BNB buyin':20} {'BNB current':20} {'ROI':20}")
+print("")
 
-    for key, value in tokens.items():
-        token_count = 0
-        bnb_current = 0
-        bnb = 0
-        for v in value:
-            # If the address has sold any tokens, they have to be subtracted from the amount. The following lines should take care of that.
-            # However, the amount sold should also be reflected in the bnb buy amount. This is still missing 
-            assert (v['from'] == ADR) or (v['to'] == ADR) # just to be sure
-            if v['to'] == ADR:
-                token_count += (float(v['value']) / (10 ** int(v['tokenDecimal'])))
-            elif v['from'] == ADR:
-                token_count -= (float(v['value']) / (10 ** int(v['tokenDecimal'])))
-            try:
-                bnb += float(bnb_values[v['hash']]) / 1000000000000000000
-            except:
-                pass
+for key, value in tokens.items():
+    token_count = 0
+    bnb_current = 0
+    bnb = 0
+    for v in value:
+        # If the address has sold any tokens, they have to be subtracted from the amount. The following lines should take care of that.
+        # However, the amount sold should also be reflected in the bnb buy amount. This is still missing 
+        assert (v['from'] == ADR) or (v['to'] == ADR)
+        if v['to'] == ADR:
+            token_count += (float(v['value']) / (10 ** int(v['tokenDecimal'])))
+        elif v['from'] == ADR:
+            token_count -= (float(v['value']) / (10 ** int(v['tokenDecimal'])))
 
-        contract = value[0]['contractAddress']
         try:
-            contract_current_values = all_tokens[contract]
-            bnb_current = float(contract_current_values['price_BNB']) * token_count
-            roi = (bnb_current * 100.0 / bnb) - 100
+            bnb += float(bnb_values[v['hash']]) / 1000000000000000000
         except:
-            bnb_current = 0.0
-            roi = 0.0
+            pass
 
-        dt_object = datetime.fromtimestamp(int(value[0]['timeStamp']))
-        bnb_total_spend += bnb
-        bnb_total_current += bnb_current
-        print(f"{key:50} {dt_object} {token_count:20.2f} {bnb:20.5f} {bnb_current:20.5} {roi:10.1f}")
+    contract = value[0]['contractAddress']
+    try:
+        contract_current_values = all_tokens[contract]
+        bnb_current = float(contract_current_values['price_BNB']) * token_count
+        roi = (bnb_current * 100.0 / bnb) - 100
+    except:
+        bnb_current = 0.0
+        roi = 0.0
 
-    print()
-    print(f"BNB spend:   {bnb_total_spend}")
-    print(f"BNB current: {bnb_total_current}")
+    dt_object = datetime.fromtimestamp(int(value[0]['timeStamp']))
+    bnb_total_spend += bnb
+    bnb_total_current += bnb_current
+    print(f"{key:50} {dt_object} {token_count:20.2f} {bnb:20.5f} {bnb_current:20.5} {roi:10.1f}")
+
+print()
+print(f"BNB spend:   {bnb_total_spend}")
+print(f"BNB current: {bnb_total_current}")
